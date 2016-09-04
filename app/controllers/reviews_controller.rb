af231ceb76c67aa4e30500dd39f4baf6bbe0317f
winfredselwyn <winfredselwyn@gmail.com>
2016-09-04 05:36:22 +0000

    added before_action :authenticate_user

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,7 @@
 class ReviewsController < ApplicationController
   before_action :set_review, only: [:show, :edit, :update, :destroy]
   before_action :set_restaurant
-  # before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /reviews
   # GET /reviews.json
