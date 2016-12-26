d71b1dcd8120a90b6606e18fef23e9fe0a625635
winfredselwyn <winfredselwyn@gmail.com>
2016-12-26 08:32:35 +0000

    removed es6 syntax

--- app/assets/javascripts/registration_validations.js
+++ app/assets/javascripts/registration_validations.js
@@ -1,5 +1,5 @@
 function validate() {
-    let valid = true;
+    var valid = true;
     valid = checkName(document.getElementById('form-register-full-name').value);
     valid = valid && checkEmail(document.getElementById('form-register-email').value);
     valid = valid && checkPassword(document.getElementById('form-register-password').value);
@@ -11,9 +11,9 @@ function validate() {
 }
 
 function checkName(obj) {
-    let result = true;
-    let inputT = /[a-zA-Z0-9]+/.test(obj);
-    let msg = document.getElementById('confirmName');
+    var result = true;
+    var inputT = /[a-zA-Z0-9]+/.test(obj);
+    var msg = document.getElementById('confirmName');
    
     if( inputT == true) {
         msg.innerHTML = '<small>' + 'Thank you ' + '</small>';
@@ -24,9 +24,9 @@ function checkName(obj) {
     }
 }
 function checkEmail(obj) {
-    let result = true;
-    let itemC = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(obj);
-    let change = document.getElementById('testEmail');
+    var result = true;
+    var itemC = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(obj);
+    var change = document.getElementById('testEmail');
     if(itemC == true) {
         change.innerHTML = '<small>' + 'Thank you!' + '</small>';
         return result = true;
@@ -37,22 +37,22 @@ function checkEmail(obj) {
 }
 
 function checkPassword(obj) {
-    let result = true;
-    let itemC = /(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/.test(obj);
-    let change = document.getElementById('testPassword');
+    var result = true;
+    var itemC = /(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/.test(obj);
+    var change = document.getElementById('testPassword');
     if(itemC == true) {
         change.innerHTML = '<small>' + 'Great password' + '</small>';
         return result = true;
     } else {
-        change.innerHTML = '<small>' + 'Passwords require a symbol, number, lowercase and uppercase letter' + '</small>';
+        change.innerHTML = '<small>' + 'Passwords require a symbol, number, lowercase and uppercase varter' + '</small>';
         return result = false;
     }
 }
 
 function checkPasswordConfirm(obj) {
-    let result = true;
-    let msg = document.getElementById('form-register-password').value;
-    let change = document.getElementById('confirmPassword2');
+    var result = true;
+    var msg = document.getElementById('form-register-password').value;
+    var change = document.getElementById('confirmPassword2');
     if(obj == msg && obj !== "") {
         change.innerHTML = '<small>' + 'Passwords match!' + '</small>';
         return result = true;
